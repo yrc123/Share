b93efdf8da986cbcbdd90aad2a7fd91bbc870483
RoseSavona <905899183@qq.com>
2021-04-14 10:19:34 +0800

    Update WordCore.java

--- LL/src/com/fzu/word/WordCore.java
+++ LL/src/com/fzu/word/WordCore.java
@@ -1,14 +1,18 @@
 package com.fzu.word;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
-public class Main {
-
+public class WordCore {
+    private List<Map<String,String>> wordList = new ArrayList<>();
+    //保留字：reservedWord  运算符：operator    标识符：id  分隔符：separator   常数:constant 错误：ERROR
     private static String inputPath;
     private static char buffer;
     private static boolean isCache=false;
@@ -21,13 +25,24 @@ public class Main {
     //运算符
     private static ArrayList<String> operator = new ArrayList<String>(
             Arrays.asList("+","-","*","/","%","++","--","-=","*=","/=","&","|","^","~","<<",">>",">>>","==","!=",
-                    ">","<","=",">=","<=","&&","||","!","."));
+                    ">","<","=",">=","<=","&&","||","!",".","[","]"));
     //分隔符
     private static ArrayList<String> separator = new ArrayList<String>(
             Arrays.asList(",",";",":","(",")","{","}"));
 
     public static void main(String[] args) throws IOException {
         inputPath=args[0];
+        WordCore wordCore=new WordCore();
+        wordCore.wordInput(inputPath);
+
+        System.out.println(wordCore.getList());
+    }
+
+    public List<Map<String,String>> getList(){
+        return wordList;
+    }
+
+    private void wordInput(String inputPath) throws IOException {
         BufferedReader br=new BufferedReader(new FileReader(inputPath));
         int cTemp;
         int status;
@@ -42,7 +57,6 @@ public class Main {
             if(cTemp==-1)
                 break;
             status=0;
-            //System.out.println((char)cTemp);
             switch (status){
                 case 0:             //第0个自动机判断ID和保留字
                     status = isId((char) cTemp, br);
@@ -58,8 +72,7 @@ public class Main {
         }
         br.close();
     }
-
-    private static int isId(char c,BufferedReader br) throws IOException {
+    private int isId(char c,BufferedReader br) throws IOException {
         if(!Character.isLetter(c))
             return 1;
         int cIntTemp;
@@ -67,7 +80,6 @@ public class Main {
         StringBuilder sb=new StringBuilder();
         sb.append(c);
         while((cIntTemp=br.read())!=-1){
-            //System.out.println((char)cIntTemp+"_");
             cTemp=(char)cIntTemp;
             if(Character.isLetterOrDigit(cTemp))
                 sb.append(cTemp);
@@ -79,17 +91,20 @@ public class Main {
         }
         if(reservedWords.contains(sb.toString())){  //如果是保留字输出保留字，不是输出ID
             System.out.println("<"+sb.toString()+","+"保留字>");
+            wordList.add(Map.of(sb.toString(),"reservedWord"));
         }
         else{
             System.out.println("<"+sb.toString()+","+"标识符>");
+            wordList.add(Map.of(sb.toString(),"id"));
         }
         return -1;
     }
 
     //判断是否为分隔符
-    private static int isSeparator(char c){
+    private int isSeparator(char c){
         if(separator.contains(String.valueOf(c))){      //是分隔符
             System.out.println("<"+c+","+"分隔符>");
+            wordList.add(Map.of(c+"","separator"));
             return -1;
         }
         else{
@@ -97,7 +112,7 @@ public class Main {
         }
     }
 
-    private static int isOperator(char c,BufferedReader br) throws IOException {
+    private int isOperator(char c,BufferedReader br) throws IOException {
         if(!operator.contains(String.valueOf(c))){      //不是操作符，直接返回
             return 3;
         }
@@ -112,14 +127,18 @@ public class Main {
                 break;
             }
         }
-        if(operator.contains(sb.toString()))
+        if(operator.contains(sb.toString())){
             System.out.println("<"+ sb.toString()+","+"操作符>");
-        else
+            wordList.add(Map.of(sb.toString(),"operator"));
+        }
+        else{
             System.out.println("<"+ sb.substring(0,sb.length()-1)+","+"操作符>");
+            wordList.add(Map.of(sb.substring(0,sb.length()-1),"operator"));
+        }
         return -1;
     }
 
-    private static int isNumber(char c,BufferedReader br) throws IOException {
+    private int isNumber(char c,BufferedReader br) throws IOException {
         if(!Character.isDigit(c))
             return 4;
         String SCIENTIFIC="([\\+|-]?\\d+(.{0}|.\\d+)?([Ee]{1}([\\+|-]?\\d+)|))";
@@ -146,9 +165,12 @@ public class Main {
         }
         if(Pattern.matches(SCIENTIFIC,sb.toString())){
             System.out.println("<"+ sb.toString()+","+"常数>");
+            wordList.add(Map.of(sb.toString(),"constant"));
         }
-        else
+        else{
             System.out.println("<"+ sb.toString()+","+"ERROR>");
+            wordList.add(Map.of(sb.toString(),"ERROR"));
+        }
         return -1;
     }
 }
